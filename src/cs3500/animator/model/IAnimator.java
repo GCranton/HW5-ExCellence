f8df22502221b0e0d34cb8dce57935c94fc78c41
jreiss1923 <joshua.reiss2@gmail.com>
2020-03-26 20:55:46 -0400

    Add files via upload

--- src/cs3500/animator/model/IAnimator.java
+++ src/cs3500/animator/model/IAnimator.java
@@ -20,7 +20,7 @@ public interface IAnimator {
   /**
    * Describes the animation as it will appear over time.
    * Description uses the following format:
-   * 
+   *
    * <p>
    * shape X [type of shape]
    * motion X [Start: [tick] [position x y] [dimensions w h] [color r g b]]
@@ -33,21 +33,21 @@ public interface IAnimator {
 
   /**
    * Adds the given shape to the animation, without any instructions yet.
-   * 
+   *
    * @param newShape the shape to be added.
    */
   public void addShape(IShape newShape);
 
   /**
    * Removes a shape from the animation, deleting all instructions associated with the shape.
-   * 
+   *
    * @param toRemove the shape to be removed
    */
   public void removeShape(IShape toRemove);
 
   /**
    * Get the full int array describing a shape at the given tick.
-   * 
+   *
    * @param shape the shape to get the description of
    * @param tick the time to describe.
    * @return an int array describing the shape at that specific tick.
@@ -57,7 +57,7 @@ public interface IAnimator {
   /**
    * Adds the given instruction to the animation, acting upon the given shape.
    * If the instruction conflicts with an earlier instruction, attempts to reconcile them.
-   * 
+   *
    * @param mod the shape the new instruction modifies.
    * @param newInst the instruction that modifies the given shape.
    */
@@ -65,7 +65,7 @@ public interface IAnimator {
 
   /**
    * Removes the instruction at the given shape's index from the animation.
-   * 
+   *
    * @param mod the shape to modify.
    * @param index the index of the Instruction to remove.
    */
@@ -73,32 +73,64 @@ public interface IAnimator {
 
   /**
    * Gets the list of shapes in the animation.
-   * 
+   *
    * @return the list of shape objects.
    */
   public List<IShape> getShapes();
 
   /**
    * Gets the list of instructions for the given shape.
-   * 
+   *
    * @param shape the shape to get the instructions for.
    * @return the list of instructions.
    */
   public List<Instruction> getInstructions(IShape shape);
 
+  /**
+   * Gets the number by which the shapes are subtracted on the y-axis.
+   * @return this.top
+   */
   public int getTop();
 
+  /**
+   * Sets top to an int.
+   * @param y the number by which top is set.
+   */
   public void setTop(int y);
 
+  /**
+   * Gets the number by which the shapes are subtracted on the x-axis.
+   * @return this.right
+   */
   public int getRight();
 
+  /**
+   * Sets right to an int.
+   * @param x the number by which right is set.
+   */
   public void setRight(int x);
 
+  /**
+   * Gets the width of the eventual JPanel.
+   * @return this.width
+   */
   public int getWidth();
 
+  /**
+   * Sets the width to an int.
+   * @param width the number by which width is set
+   */
   public void setWidth(int width);
 
+  /**
+   * Gets the height of the eventual JPanel.
+   * @return this.height
+   */
   public int getHeight();
 
+  /**
+   * Sets the height to an int.
+   * @param height the number by which height is set.
+   */
   public void setHeight(int height);
 }
